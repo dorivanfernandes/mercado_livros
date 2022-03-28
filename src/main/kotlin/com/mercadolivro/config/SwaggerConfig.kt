2dce140a1696d79b9cf4d7f350ec17bb4ce9e97c
Dorivan Fernandes <dorivan.duarte@creditas.com>
2022-03-28 16:49:30 -0300

    teste

--- src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -14,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
+
     @Bean
     fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
         .select()
